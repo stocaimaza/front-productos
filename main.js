63cfdeb93574848fbc98e06fb66f2b2fbf00dc66
stocaimaza <stocaimaza@hotmail.com>
2024-11-15 20:28:02 -0300

    carga inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+//Consumimos datitos de un backend propio: 
+
+const contenedorProductos = document.getElementById("contenedorProductos"); 
+
+fetch("http://localhost:8080/productos")
+    .then(response => response.json() )
+    .then(data => {
+        renderizarProductos(data);
+    })
+    .catch(error => console.log(error))
+
+
+function renderizarProductos(arrayProductos) {
+    const card = document.createElement("card"); 
+    
+    arrayProductos.forEach(item => {
+        card.innerHTML +=`
+                        <div class= "caja">
+                            <p>ID: ${item.id} </p>
+                            <p>Nombre: ${item.nombre} </p>
+                            <p>Precio: ${item.precio} </p> 
+                            <button class= "btn" > Comprar </button>  
+                        </div>    
+                        `
+    })
+
+    contenedorProductos.appendChild(card);
+}
